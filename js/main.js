161d7eda2f05170bd3c2a6c336ace1503719069d
Mariano Rodriguez Domato <mariano.rdomato@gmail.com>
2021-03-17 17:14:33 -0300

    first

---
+++ js/main.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  function focus() {
+    $("#user").focus();
+  }
+  $(".terminal").show();
+  focus();
+  // Focus, focus everywhere.
+  $(window).focus(function () {
+    focus();
+    $("#user").val($("#user").val());
+  });
+  $(document).click(function (e) {
+    focus();
+    $("#user").val($("#user").val());
+  });
+  $("#user").on("input", function (e) {
+    $(".clone").text($(this).val());
+  });
+  // Si es usuario presiona alguna tecla
+  $("#user").keypress(function (e) {
+    // Si es usuario presiona enter
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      var valor = $(this).val(); // Rescatamos valor
+      $(this).val(""); // Hacemos clear al input
+      $(".clone").text(""); // Hacemos clear al clon
+      $("#respuesta").append('<p class="comando">' + valor + "</p>"); // Simulamos metida del comando en contenedor
+      caso = cases.indexOf(valor); // views.js -> tomamos valor del array
+      if (caso === -1) {
+        // Si no existe el comando regresa -1
+        $("#respuesta").append(
+          '<p class="respuesta">Comando "' +
+            valor +
+            '" no identificado.<br />Para ver lista de comandos, escribe: help</p>'
+        );
+      } else {
+        terminal(caso); // Si existe regresa 1..oo, ejecutamos función terminal
+      }
+    }
+  });
+  function terminal(caso) {
+    // Comando es un objeto, accedemos al caso
+    if (comando[caso]) {
+      // Si el caso tiene valor
+      var x = comando[caso]; // asignamos a variable
+      $("#respuesta").append('<p class="respuesta">' + x + "</p>"); // Simulamos respuesta
+    } else {
+      // Si no, es el primer comando, es decir, comando clear (limpia pantalla)
+      $("#respuesta, .info").empty(); // Pasamos un trapito
+    }
+  }
+  // Función para hacer blink al cursor
+  // http://stackoverflow.com/questions/12903594/set-css-with-jquery-after-ajax
+  $(".blink").each(function () {
+    var elem = $(this);
+    setInterval(function () {
+      if (elem.css("visibility") == "hidden") {
+        elem.css("visibility", "visible");
+      } else {
+        elem.css("visibility", "hidden");
+      }
+    }, 500);
+  });
+  // Para tomar la barra superior, simulamos drag en mouse
+  $("#handle")
+    .live("mousedown", function () {
+      // Click!
+      $(this).css("cursor", "move"); // Drag
+    })
+    .live("mouseup", function () {
+      // Click-off...
+      $(this).css("cursor", "default"); // Default
+    });
+  // Handler con dom-drag.js
+  // DYNAMIC DRIVE FUCK YEAH!
+  // http://www.dynamicdrive.com/dynamicindex11/domdrag/
+  var theHandle = document.getElementById("handle");
+  var theRoot = document.getElementById("terminal");
+  Drag.init(theHandle, theRoot);
+});
---
+++ js/views.js
@@ -0,0 +1,19 @@
+var cases = [
+  "clear",
+  "help",
+  "about me",
+  "skills",
+  "cv",
+  "github",
+  "studies",
+  "courses",
+]; // Casos
+var comando = {
+  1: "Comandos disponibles:<br>clear<br>help<br>about me<br>skills<br>cv<br>github<br>studies<br>courses",
+  2: "Hola! Soy Mariano Rodriguez Domato y vivo en Buenos Aires, Argentina.<br>Soy Analista de Sistema | Software Developer | Entusiasta de la Seguridad Informatica<br>Me considero una persona apasionada por la tecnologia en todas sus ramas, busco comprender a los usuarios, entender sus necesidades y asi poder ofrecerles la mejor solución posible con las herramientas adecuadas. <br>Decidi crear este portfolio dinamico e interactivo que simula una terminal uniendo asi dos de mis pasiones, el desarrollo web y la seguridad informatica.<br> Interactuando con la terminal a traves de los comandos disponibles podra visitar mi Linkedin, Github e incluso descargar mi curriculum vitae!<br> Espero que disfrute esta pagina tanto como yo al hacerla!", //Busco comprender a los usuarios, entender sus necesidades y poder ofrecerles la mejor solución posible con las herramientas adecuadas, generando un valor agregado al trabajo ﬁnal.
+  3: "Ingles (Avanzado) <br> HTML, CSS y JavaScript <br> ReactJs <br> WordPress, WooComerce y PrestaShop <br> MySQL y MongoDB <br> Java (Android Nativo) | GO | R y Python | Metodologias Agiles | Firebase |",
+  4: '<script> window.open("../files/Mariano Rodriguez Domato - CV.pdf")</script>',
+  5: '<script> window.open("https://github.com/mdomato")</script>',
+  6: "Analista de Sistema, Instituto Tecnologico ORT (recibido en julio del 2020)<br> Ingenieria en Informatica, Univerdad del CEMA (inicio en marzo del 2022) ",
+  7: "",
+}; // Respuesta a comandos
