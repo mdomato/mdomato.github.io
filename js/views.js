b0e5e63f016ba07a576a7226e714eb9b8d40fd90
mdomato <mariano.rdomato@gmail.com>
2021-07-01 22:53:58 -0300

    update

--- js/views.js
+++ js/views.js
@@ -2,18 +2,16 @@ var cases = [
   "clear",
   "help",
   "about me",
+  "studies",
   "skills",
   "cv",
   "github",
-  "studies",
-  "courses",
 ]; // Casos
 var comando = {
-  1: "Comandos disponibles:<br>clear<br>help<br>about me<br>skills<br>cv<br>github<br>studies<br>courses",
-  2: "Hola! Soy Mariano Rodriguez Domato y vivo en Buenos Aires, Argentina.<br>Soy Analista de Sistema | Software Developer | Entusiasta de la Seguridad Informatica<br>Me considero una persona apasionada por la tecnologia en todas sus ramas, busco comprender a los usuarios, entender sus necesidades y asi poder ofrecerles la mejor solución posible con las herramientas adecuadas. <br>Decidi crear este portfolio dinamico e interactivo que simula una terminal uniendo asi dos de mis pasiones, el desarrollo web y la seguridad informatica.<br> Interactuando con la terminal a traves de los comandos disponibles podra visitar mi Linkedin, Github e incluso descargar mi curriculum vitae!<br> Espero que disfrute esta pagina tanto como yo al hacerla!", //Busco comprender a los usuarios, entender sus necesidades y poder ofrecerles la mejor solución posible con las herramientas adecuadas, generando un valor agregado al trabajo ﬁnal.
-  3: "Ingles (Avanzado) <br> HTML, CSS y JavaScript <br> ReactJs <br> WordPress, WooComerce y PrestaShop <br> MySQL y MongoDB <br> Java (Android Nativo) | GO | R y Python | Metodologias Agiles | Firebase |",
-  4: '<script> window.open("../files/Mariano Rodriguez Domato - CV.pdf")</script>',
-  5: '<script> window.open("https://github.com/mdomato")</script>',
-  6: "Analista de Sistema, Instituto Tecnologico ORT (recibido en julio del 2020)<br> Ingenieria en Informatica, Univerdad del CEMA (inicio en marzo del 2022) ",
-  7: "",
+  1: "Comandos disponibles:<br>clear<br>help<br>about me<br>studies<br>skills<br>cv<br>github",
+  2: "Hola! Soy Mariano Rodriguez Domato y vivo en Buenos Aires, Argentina.<br>Soy Analista de Sistema | Software Developer | Entusiasta de la Seguridad Informática<br>Me considero una persona apasionada por la tecnología en todas sus ramas, busco comprender a los usuarios, entender sus necesidades y así poder ofrecerles la mejor solución posible con las herramientas adecuadas. <br>Decidí crear este portfolio dinámico e interactivo que simula una terminal uniendo así dos de mis pasiones, el desarrollo web y la seguridad informática.<br> Interactuando con la terminal a través de los comandos disponibles podrá visitar mi Linkedin, Github e incluso descargar mi curriculum vitae!<br> Espero que disfrute esta página tanto como yo al hacerla!", 
+  3: "Analista de Sistema, Instituto Tecnológico ORT (recibido en julio del 2020)<br> Ingeniería en Informática, Univerdad del CEMA (en curso) ",
+  4: "Inglés (Intermedio) <br> HTML, CSS y JavaScript <br> ReactJs <br> WordPress, WooComerce y PrestaShop <br> MySQL y MongoDB <br> Java (JDBC, Servlet, Spring) y Android Nativo | GO | R y Python | Metodologias Agiles | Firebase |",
+  5: '<script> window.open("../files/Mariano Rodriguez Domato - CV.pdf")</script>',
+  6: '<script> window.open("https://github.com/mdomato")</script>',
 }; // Respuesta a comandos
