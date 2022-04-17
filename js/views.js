b24e6a757aacc973e90d0a9aebe7fa4c60f70dee
Mariano Rodriguez <mrodriguez@certant.com>
2022-04-17 15:29:55 -0300

    updating job profile fix

--- js/views.js
+++ js/views.js
@@ -8,9 +8,9 @@ var cases = [
   "cv",
 ]; // Casos
 var comando = {
-  1: "Comandos disponibles:<br>clear<br>help<br>about me<br>studies<br>skills<br>cv<br>github",
+  1: "Comandos disponibles:<br>clear<br>help<br>about me<br>jobs<br>studies<br>skills<br>cv",
   2: "Hola! Soy Mariano Rodriguez Domato y vivo en Buenos Aires, Argentina.<br>Soy Analista de Sistema | Software Developer | Entusiasta de la Seguridad Informática<br>Me considero una persona apasionada por la tecnología en todas sus ramas, busco comprender a los usuarios, entender sus necesidades y así poder ofrecerles la mejor solución posible con las herramientas adecuadas. <br>Decidí crear este portfolio dinámico e interactivo que simula una terminal uniendo así dos de mis pasiones, el desarrollo web y la seguridad informática.<br> Interactuando con la terminal a través de los comandos disponibles podrá visitar mi Linkedin, Github e incluso descargar mi Curriculum Vitae!<br> Espero que disfrute esta página tanto como yo al hacerla!", 
-  3: "Software Developer en Certant (Agosto 2021 - Actualidad) <br> Proyecto para un cliente bancario del exterior en el que desarrollé tareas de mantenimiento y nuevos features, tanto de frontend como de backend <br> Tecnologías: Java, Spring, HTML, CSS, Jquery. Javascript, MQ Queue, JUnit, Mockito. <br> Metodología: Scrum con uso de Jira como Kanban.",
+  3: '"Software Developer at Certant (Agosto 2021 - Actualidad) <br> Proyecto para un cliente bancario del exterior en el que desarrollé tareas de mantenimiento y nuevos features, tanto de frontend como de backend <br> Tecnologías: Java, Spring, HTML, CSS, Jquery. Javascript, MQ Queue, JUnit, Mockito. <br> Metodología: Scrum con uso de Jira como Kanban.",',
   4: "Analista de Sistemas, Instituto Tecnológico ORT (recibido en julio del 2020 - promedio de 8)<br> Ingeniería en Informática, Univerdad del CEMA (en curso) ",
   5: "Inglés - oral y escrito (Intermedio) <br> HTML, CSS y JavaScript (también Jquery) <br> ReactJs <br> WordPress<br> MySQL, MongoDB y Firebase <br> Java 7 y 8 (JDBC, JPA, Servlets, Spring, SpringBoot, etc) y Android Nativo con Java <br> GO (básico) | R y Python (básico) <br> Metodologias Agiles con Jira como Kanban <br> Linux (Kali e incursionando en Parrot)",
   6: '<script> window.open("../files/MarianoRodriguezDomatoCV.pdf")</script>',
